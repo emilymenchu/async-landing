1c8c0ce01a8fbf7595c29e0fb3079d37f4af5205
Emily Menchu <menchu.lucila@gmail.com>
2024-07-23 08:36:23 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -35,19 +35,20 @@ async function fetchData(url, options) {
         const videos = await fetchData(urlVideo, optionsVideo);
         let view = `
         ${videos.items.map(video => `
-            <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+                <div class="group relative">
+                    <div
+                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                        </h3>
+                    </div>
                 </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${video.snippet.title}
-                    </h3>
-                </div>
-            </div>
-
+            </a>
         `).join('')}
         `;
     content.innerHTML = view;
